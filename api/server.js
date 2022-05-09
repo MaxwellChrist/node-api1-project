42e46edaf45c9d55da4263d9368eecd1611f2b28
MaxwellChrist <ribthulu@gmail.com>
2022-05-09 16:32:24 -0400

    completed post and get requests

--- api/server.js
+++ api/server.js
@@ -1,3 +1,52 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const Data = require('../api/users/model.js');
+const server = express();
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use(express.json());
+
+server.post('/api/users', (req, res) => {
+    const {name, bio} = req.body;
+    Data.insert(req.body)
+    .then(result => {
+        if (name == null || name == "") {
+            res.status(400).json({ message: "Please provide a name for the user" })
+        } else if (bio == null || bio == "") {
+            res.status(400).json({ message: "Please provide a bio for the user" })
+        } else {
+            res.status(201).json(result)
+        }
+    })
+    .catch(result => {
+        res.status(500).json({ message: "There was an error while saving the user to the database" })
+    })
+})
+
+server.get('/api/users', (req, res) => {
+    Data.find()
+    .then(result => {
+        res.json(result)
+    })
+    .catch(result => {
+        res.status(500).json({ message: "The users information could not be retrieved" })
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    Data.findById(id)
+    .then(result => {
+        if (result == null) {
+            res.status(404).json({ message:"The user with the specified ID does not exist" })
+        } else {
+            res.json(result)
+        }
+    })
+    .catch(result => {
+        res.status(500).json({ message: "The user information could not be retrieved"  })
+    })
+})
+
+
+
+module.exports = server
