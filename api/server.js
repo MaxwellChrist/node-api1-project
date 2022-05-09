08543c85d0ce66181db5a725933ef7f42444ad14
MaxwellChrist <ribthulu@gmail.com>
2022-05-09 16:53:51 -0400

    completed delete request

--- api/server.js
+++ api/server.js
@@ -7,19 +7,17 @@ server.use(express.json());
 
 server.post('/api/users', (req, res) => {
     const {name, bio} = req.body;
-    Data.insert(req.body)
-    .then(result => {
-        if (name == null || name == "") {
-            res.status(400).json({ message: "Please provide a name for the user" })
-        } else if (bio == null || bio == "") {
-            res.status(400).json({ message: "Please provide a bio for the user" })
-        } else {
+    if (!name || !bio) {
+        res.status(400).json({ message: "Please provide name and bio for the user" })
+    } else {
+        Data.insert(req.body)
+        .then(result => {
             res.status(201).json(result)
-        }
-    })
-    .catch(result => {
-        res.status(500).json({ message: "There was an error while saving the user to the database" })
-    })
+        })
+        .catch(result => {
+            res.status(500).json({ message: "There was an error while saving the user to the database" })
+        })
+    }
 })
 
 server.get('/api/users', (req, res) => {
@@ -44,6 +42,21 @@ server.get('/api/users/:id', (req, res) => {
     })
     .catch(result => {
         res.status(500).json({ message: "The user information could not be retrieved"  })
+    })
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    Data.remove(id)
+    .then(result => {
+        if (result == null) {
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        } else {
+            res.json(result)
+        }
+    })
+    .catch(result => {
+        res.status(500).json({ message: "The user could not be removed"})
     })
 })
 
